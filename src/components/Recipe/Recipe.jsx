5cb2e5ef392c0b7de09093a63f929667e02549e5
Farhad2590 <farhadhossen2590@gmail.com>
2024-03-18 00:13:20 +0600

    Design updated

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,7 +1,7 @@
 const Recipe = () => {
     return (
         <div className="flex flex-col items-center gap-3 mt-12">
-            <h1 className="text-2xl font-bold text-center text-green-400">Our Recipes</h1>
+            <h1 className="text-2xl font-bold text-center text-green-400">Our Culinary Creations</h1>
             <p className="w-[670px] text-center">Indulge in a flavorful journey with our diverse range of Calorie-Savvy Dishes, meticulously crafted to tantalize your taste buds while keeping your health in mind.</p>
             
         </div>
